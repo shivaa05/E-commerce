0d0ccdaa9b535be4aa3803cbd3892d5e12b79c4d
Shiva Verma <shivaverma3211.a@gmail.com>
2026-04-13 17:37:15 +0530

    Navbar update

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -52,6 +52,12 @@ const userSchema = new mongoose.Schema({
         default: 1,
       },
     },
+  ],
+  wishList: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+    },
   ]
 });
 
